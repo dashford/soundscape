a670341ee2649a6e4c2b6f5d4fce583be40e749b
David Ashford <david@dashford.io>
2020-04-26 21:29:46 +0100

    move to mysql dependency

--- web/index.php
+++ web/index.php
@@ -1,11 +1,42 @@
 <?php
 
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$env = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$env->load();
+
+
+// Doctrine Config
+$paths = array(__DIR__ . '/../src');
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = [
+    'driver'   => 'pdo_mysql',
+    'user'     => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'dbname'   => getenv('DB_NAME'),
+];
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+
+// App Config
+$container = new Container();
+
+$container->set('entityManager', function () use ($entityManager) {
+    return $entityManager;
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
